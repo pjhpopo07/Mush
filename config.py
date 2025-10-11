109886cc377e8e76e501535699fa46ddb4be4c13
pjhpopo07 <pjhpopo07@gmail.com>
2025-10-11 17:34:38 +0900

    버섯 종류 수 변경

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # 이미지 및 모델 설정
 IMAGE_SIZE = (299, 299)
 NUM_CHANNELS = 3 # RGB 이미지이므로 3
-NUM_CLASSES = 2  # 분류할 버섯 종류의 수에 맞게 변경하세요!
+NUM_CLASSES = 5  # 분류할 버섯 종류의 수에 맞게 변경하세요!
 
 # 데이터 경로 및 준비
 IMAGE_FOLDER = 'your_mushroom_images' # 원본 이미지가 있는 폴더
