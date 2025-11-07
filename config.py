07be77f393a08a358c61055f91693e410ac0a880
pjhpopo07 <pjhpopo07@gmail.com>
2025-11-07 18:28:39 +0900

    모델 학습 후 판독 테스트

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ NUM_CLASSES = 10  # 분류할 버섯 종류의 수에 맞게 변경하세요!
 
 # 데이터 경로 및 준비
 IMAGE_FOLDER = 'your_mushroom_images' # 원본 이미지가 있는 폴더
-BATCH_SIZE = 32
+BATCH_SIZE = 4
 VALIDATION_SPLIT = 0.2
 
 # 훈련 설정
-EPOCHS = 10
-LEARNING_RATE = 0.001
+EPOCHS = 20
+LEARNING_RATE = 0.0001
